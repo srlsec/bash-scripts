f90bf80e0c23bb7e6df62f258c54f7849cb254d3
Sarathlal U P <56441804+srlsec@users.noreply.github.com>
2026-04-18 14:39:26 +0530

    Create vscode-install.sh

---
+++ vscode-install.sh
@@ -0,0 +1,14 @@
+# Update your system and install required tools
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget -y
+
+# Import Microsoft's GPG key for package verification
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+
+# Add the official VS Code repository
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Install VS Code
+sudo apt update
+sudo apt install code
